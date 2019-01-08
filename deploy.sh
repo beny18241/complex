6231892fbafb40825cdb3d6e64c418d4c0a13b0c
Maciej Pindela <beny18241@gmail.com>
2019-01-08 19:58:45 +0100

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,18 @@
 #beny18241
-docker build -t beny18241/complex-client -f ./client/Dockerfile ./client
-docker build -t beny18241/complex-server -f ./server/Dockerfile ./server
-docker build -t beny18241/complex-worker -f ./worker/Dockerfile ./worker
+docker build -t beny18241/complex-client:latest -t beny18241/complex-client:$SHA -f ./client/Dockerfile ./client
+docker build -t beny18241/complex-server:latest -t beny18241/complex-server:$SHA -f ./server/Dockerfile ./server
+docker build -t beny18241/complex-worker:latest -t beny18241/complex-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push beny18241/complex-client
-docker push beny18241/complex-server
-docker push beny18241/complex-worker
+docker push beny18241/complex-client:latest
+docker push beny18241/complex-server:latest
+docker push beny18241/complex-worker:latest
+
+docker push beny18241/complex-client:$SHA
+docker push beny18241/complex-server:$SHA
+docker push beny18241/complex-worker:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/server-deployment server=beny18241/complex-server
+kubectl set image deployments/server-deployment server=beny18241/complex-server:$SHA
+kubectl set image deployments/client-deployment client=beny18241/complex-client:$SHA
+kubectl set image deployments/worker-deployment worker=beny18241/complex-worker:$SHA
